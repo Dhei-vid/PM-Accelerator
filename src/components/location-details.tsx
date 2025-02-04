96f489e584a0194ca4ad9d32e64d00b3dc4dd149
Dhei-vid <djetuk@gmail.com>
2025-02-04 13:07:45 +0100

    fix: image config

--- src/components/location-details.tsx
+++ src/components/location-details.tsx
@@ -10,29 +10,35 @@ interface ILocationDetails {
 const LocationDetails: FC<ILocationDetails> = ({ data }) => {
   const formatedDate = format(
     new Date(data?.location?.localtime as unknown as string),
-    "p - EEEE, ee LLL YYYY"
+    "p - EEEE, ee LLL yyyy"
   );
-  const date = new Date(data?.location?.localtime as unknown as string);
+
   return (
     <div className={"flex flex-row justify-between p-12 items-center"}>
       <div>
-        <p className={"text-6xl font-bold"}>°{data?.current?.temp_c}C</p>
+        <p className={"text-6xl font-bold text-primary"}>
+          °{data?.current?.temp_c}C
+        </p>
       </div>
       <div className={"flex flex-col gap-2"}>
-        <p className={"text-2xl font-semibold"}>{data?.location?.name}</p>
+        <p className={"text-2xl font-semibold text-primary"}>
+          {data?.location?.name}
+        </p>
         <div>
-          <p className={"text-base"}>{formatedDate}</p>
+          <p className={"text-base text-primary"}>{formatedDate}</p>
         </div>
       </div>
 
       <div>
-        {/* <Image
+        <Image
           width={40}
           height={40}
           src={`https:${data?.current?.condition?.icon as string}`}
           alt={`${data?.current?.condition?.text} weather icon`}
-        /> */}
-        <p>{data?.current?.condition?.text}</p>
+        />
+        <p className={"text-lg text-primary"}>
+          {data?.current?.condition?.text}
+        </p>
       </div>
     </div>
   );
